459342caeeb099e6f33016252fb50f82c8fbc32a
uitwaaien6 <ruzgarata6@gmail.com>
2021-07-19 17:19:34 +0300

    nav bar improvements responsive

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,122 +1,168 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { FaTwitter } from 'react-icons/fa';
+
 import cn from 'classnames';
+import { GiFeather } from 'react-icons/gi';
+import {
+  FaTwitter as TwitterIcon,
+  FaHashtag as ActiveHashtagIcon,
+} from 'react-icons/fa';
+import { BiHomeCircle as HomeIcon } from 'react-icons/bi';
+import { RiHome7Fill as ActiveHomeIcon } from 'react-icons/ri';
+import {
+  BsBell as BellIcon,
+  BsBellFill as ActiveBellIcon,
+} from 'react-icons/bs';
+
+import {
+  MdMailOutline as MailIcon,
+  MdEmail as ActiveMailIcon,
+} from 'react-icons/md';
+
+import {
+  BsFillBookmarkFill as ActiveBookmarkIcon,
+  BsBookmark as BookmarkIcon,
+} from 'react-icons/bs';
+
+import {
+  RiFileList2Line as ListIcon,
+  RiFileList2Fill as ActiveListIcon,
+} from 'react-icons/ri';
+
+import { IoPersonOutline as PersonIcon } from 'react-icons/io';
+import { BsPersonFill as ActivePersonIcon } from 'react-icons/bs';
+import { CgMoreO as MoreIcon } from 'react-icons/cg';
 
 // STYLES
 import styles from './Nav.module.scss';
 
 function Nav() {
+  const [activeIcon, setActiveIcon] = useState(-1);
+
+  const navItems = [
+    {
+      activeIcon: <ActiveHomeIcon />,
+      icon: <HomeIcon />,
+      title: 'Home',
+      path: '/home',
+    },
+    {
+      activeIcon: <ActiveHashtagIcon />,
+      icon: <ActiveHashtagIcon />,
+      title: 'Explore',
+      path: '/explore',
+    },
+    {
+      activeIcon: <ActiveBellIcon />,
+      icon: <BellIcon />,
+      title: 'Notifications',
+      path: '/notifications',
+    },
+    {
+      activeIcon: <ActiveMailIcon />,
+      icon: <MailIcon />,
+      title: 'Messages',
+      path: '/messages',
+    },
+    {
+      activeIcon: <ActiveBookmarkIcon />,
+      icon: <BookmarkIcon />,
+      title: 'Bookmarks',
+      path: '/bookmarks',
+    },
+    {
+      activeIcon: <ActiveListIcon />,
+      icon: <ListIcon />,
+      title: 'Lists',
+      path: '/lists',
+    },
+    {
+      activeIcon: <ActiveHomeIcon />,
+      icon: <ActivePersonIcon />,
+      title: 'Profile',
+      path: '/profile',
+    },
+    {
+      activeIcon: <MoreIcon />,
+      icon: <MoreIcon />,
+      title: 'More',
+    },
+  ];
+
+  useEffect(() => {
+    console.log(activeIcon);
+  }, [activeIcon]);
+
   return (
     <>
       <nav>
         <ul>
           <li>
-            <Link to="/home">
-              <div className={cn(styles['list-item-container'])}>
-                <div className={styles['main-icon']}>
-                  <FaTwitter />
+            <div
+              className={cn(
+                styles['list-item-container'],
+                styles['reset'],
+              )}
+            >
+              <Link to="/home">
+                <div className={cn(styles['main-icon'])}>
+                  <TwitterIcon />
                 </div>
-              </div>
-            </Link>
+              </Link>
+            </div>
           </li>
-          <li>
-            <Link to="/home">
-              <div className={styles['list-item-container']}>
-                <div className={styles['icon']}>
-                  <FaTwitter />
-                </div>
+          {navItems.map((navItem, index) => {
+            return (
+              <li key={index}>
+                <div
+                  className={cn(
+                    styles['list-item-container'],
+                    index === activeIcon
+                      ? styles['active-item']
+                      : null,
+                  )}
+                >
+                  <Link
+                    onClick={() => setActiveIcon(index)}
+                    to={navItem.path && navItem.path}
+                  >
+                    <div className={styles['icon']}>
+                      {index === activeIcon
+                        ? navItem.activeIcon
+                        : navItem.icon}
+                    </div>
 
-                <div className={styles['title']}>Home</div>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link>
-              <div className={styles['list-item-container']}>
-                <div className={styles['icon']}>
-                  <FaTwitter />
+                    <div className={styles['title']}>
+                      {navItem.title}
+                    </div>
+                  </Link>
                 </div>
+              </li>
+            );
+          })}
 
-                <div className={styles['title']}>Explore</div>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link>
-              <div className={styles['list-item-container']}>
-                <div className={styles['icon']}>
-                  <FaTwitter />
-                </div>
-
-                <div className={styles['title']}>Notifications</div>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link>
-              <div className={styles['list-item-container']}>
-                <div className={styles['icon']}>
-                  <FaTwitter />
-                </div>
-
-                <div className={styles['title']}>Messages</div>
-              </div>
-            </Link>
-          </li>
           <li>
             <Link>
-              <div className={styles['list-item-container']}>
-                <div className={styles['icon']}>
-                  <FaTwitter />
-                </div>
+              <button>
+                <span>Tweet</span>
 
-                <div className={styles['title']}>Bookmarks</div>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link>
-              <div className={styles['list-item-container']}>
-                <div className={styles['icon']}>
-                  <FaTwitter />
+                <div className={styles['feather']}>
+                  <GiFeather />
                 </div>
-
-                <div className={styles['title']}>Lists</div>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link>
-              <div className={styles['list-item-container']}>
-                <div className={styles['icon']}>
-                  <FaTwitter />
-                </div>
-
-                <div className={styles['title']}>Profile</div>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link>
-              <div className={styles['list-item-container']}>
-                <div className={styles['icon']}>
-                  <FaTwitter />
-                </div>
-
-                <div className={styles['title']}>More</div>
-              </div>
+              </button>
             </Link>
           </li>
         </ul>
 
         <div className={styles['account']}>
-          <img />
-          <div className={styles['/info']}>
-            <div className={styles['name']}>Ruzgar</div>
+          <img src="/assets/img/ruzgar.JPG" />
+          <div className={styles['info']}>
+            <div className={styles['name']}>$</div>
             <div className={styles['username']}>@RuzgarAtaOzkan</div>
           </div>
-          <div>...</div>
+          <div className={styles['more-options']}>
+            <MoreIcon />
+          </div>
         </div>
       </nav>
     </>
