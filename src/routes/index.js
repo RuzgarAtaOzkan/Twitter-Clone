b295704043051cd0b8891e07022ce398e6a2439e
RuzgarAtaOzkan <ruzgarataozkan@gmail.com>
2021-11-25 18:51:57 +0300

    AuthLayout add, config endpoints add

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,40 +6,69 @@ import {
   Route,
 } from 'react-router-dom';
 
+// CONFIG
+import routesConfig from '../config/routes/index';
+
 // COMPONENTS
 import GlobalComponents from '../components/Global';
 
 // LAYOUTS
 import FeedLayout from '../components/Layouts/Feed';
 import SettingsLayout from '../components/Layouts/Settings';
+import AuthLayout from '../components/Layouts/Auth';
 
 // PAGES
 import LoadingPage from '../pages/Loading';
 
-// LAZY LOADING PAGES
+// LAZY PAGES
+
+// Root pages
 const RootPage = lazy(() => import('../pages/Root.js'));
+
+// Auth layout pages
+const SigninPage = lazy(() => import('../pages/Signin.js'));
+
+// Feed layout ages
 const HomePage = lazy(() => import('../pages/Home.js'));
 const ExplorePage = lazy(() => import('../pages/Explore.js'));
 const NotificationsPage = lazy(() =>
   import('../pages/Notifications.js'),
 );
+
+// Settings layout pages
 const MessagesPage = lazy(() => import('../pages/Messages.js'));
 
 function Routes() {
+  const endpoints = { ...routesConfig.endpoints };
+
   return (
     <>
       <Suspense fallback={<LoadingPage />}>
         <Router>
           <Switch>
-            <Route exact path="/" component={RootPage} />
-            <FeedLayout path="/home" component={HomePage} />
-            <FeedLayout path="/explore" component={ExplorePage} />
+            {/* Root route */}
+            <Route exact path={endpoints.root} component={RootPage} />
+
+            {/* Auth layouts */}
+            <AuthLayout
+              path={endpoints.signin}
+              component={SigninPage}
+            />
+
+            {/* Feed Layouts, e.g. Home page, Notifications page */}
+            <FeedLayout path={endpoints.home} component={HomePage} />
             <FeedLayout
-              path="/notifications"
+              path={endpoints.explore}
+              component={ExplorePage}
+            />
+            <FeedLayout
+              path={endpoints.notifications}
               component={NotificationsPage}
             />
+
+            {/* Settings Layouts */}
             <SettingsLayout
-              path="/messages"
+              path={endpoints.messages}
               component={MessagesPage}
             />
           </Switch>
