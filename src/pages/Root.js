c7aa35e6e0abf8bba7b123fada7d1574cdf0c9dc
RuzgarAtaOzkan <ruzgarataozkan@gmail.com>
2021-11-25 18:50:39 +0300

    config endpoints add

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -3,14 +3,19 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
+// CONFIG
+import routesConfig from '../config/routes';
+
 function Root() {
+  const endpoints = { ...routesConfig.endpoints };
+
   const auth = useSelector((state) => state.auth);
 
   if (!auth.user || !auth.user.authorized) {
-    return <Redirect to="/login" />;
+    return <Redirect to={endpoints.signin} />;
   }
 
-  return <Redirect to="/home" />;
+  return <Redirect to={endpoints.home} />;
 }
 
 export default Root;
